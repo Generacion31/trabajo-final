c108d79191a48d63746fefba58a1a008e778baf3
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-10-12 14:20:00 -0300

    advances

--- productSpecification.js
+++ productSpecification.js
@@ -1,8 +1,9 @@
 import back from "./src/components/back/back.js";
+import cart from "./src/components/cart/cart.js";
 import detail from "./src/components/detail/detail.js";
 import nav from "./src/components/nav/nav.js";
 
 nav()
 back()
 detail()
-//console.log(localStorage.getItem('productModal'));
+cart()
